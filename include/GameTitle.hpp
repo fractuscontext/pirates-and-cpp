b98881148b70a5d53a5703dccbde84fae9c0bf5f
fractuscontext <106440141+fractuscontext@users.noreply.github.com>
2026-03-22 02:02:39 +1030

    chore(ui): add GameTitle splash screen branding

---
+++ include/GameTitle.hpp
@@ -0,0 +1,29 @@
+/**
+ * @file CannonCard.hpp
+ * @brief Declares the CannonCard class for the Cannon suit.
+ *
+ * Created by Andy Cunningham on 23/3/2025.
+ * Updated by Claire Tam on 21/3/2026.
+ *
+ * SPDX-FileCopyrightText: 2026 Claire Tam <claire.tam@student.adelaide.edu.au>
+ * SPDX-FileCopyrightText: 2026 fractuscontext (aka Claire T.) <106440141+fractuscontext@users.noreply.github.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef CANNON_CARD_HPP
+#define CANNON_CARD_HPP
+
+#define GAME_TITLE                                                             \
+    "______                  _   ___  ___              _     \n"               \
+    "|  _  \\                | |  |  \\/  |             ( )    \n"             \
+    "| | | | ___   __ _   __| |  | .  . |  __ _  _ __ |/ ___ \n"               \
+    "| | | |/ _ \\ / _` | / _` |  | |\\/| | / _` || '_ \\  / __|\n"            \
+    "| |/ /|  __/| (_| || (_| |  | |  | || (_| || | | | \\__ \\ \n"            \
+    "______ \\___| \\__,_| \\__,_|  \\_|  |_/ \\__,_||_| |_| |___/\n"          \
+    "|  _  \\                         _      _\n"                              \
+    "| | | | _ __  __ _ __      __ _| |_  _| |_\n"                             \
+    "| | | || '__|/ _` |\\ \\ /\\ / /|_   _||_   _|\n"                         \
+    "| |/ / | |  | (_| | \\ V  V /   |_|    |_|\n"                             \
+    "|___/  |_|   \\__,_|  \\_/\\_/\n"
+
+#endif // CANNON_CARD_HPP
